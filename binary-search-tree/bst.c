86f766a9aa8dd8b728aa10de40bd630f65e0ae7b
lhahner <71670918+lhahner@users.noreply.github.com>
2024-02-14 17:58:24 +0100

    Add files via upload

---
+++ binary-search-tree/bst.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct Node{
+	int key;
+	struct Node *left;
+	struct Node *right;
+};
+
+Node addNode(struct Node *obj, int _key){
+	obj = malloc(sizeof(Node));
+	if(obj == NULL){
+		obj.key = _key;
+		obj.left = NULL;
+		obj.right = NULL;
+	}
+	else {
+	 	if(obj.key < _key){
+			tree.left = 
+		}		
+	}
+}
+
+
+
+void main(){
+	
+}
